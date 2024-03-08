30e90e518327ddf828b25ae44342455a5fdc041d
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-03-08 16:19:14 -0300

    prova modulo 5

--- projeto-web/main.py
+++ projeto-web/main.py
@@ -1,22 +1,44 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for
 from tinydb import TinyDB
 
 app = Flask(__name__)
 
-db = TinyDB("posts.json")
+db = TinyDB("caminhos.json")
 
+# rota para a página inicial
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/sobre", methods=["GET", "POST"])
-def sobre(nome=None):
+# rota para cadastrar novas posições
+@app.route("/cadastro_new", methods=["POST"])
+def cadastro_new():
     if request.method == "POST":
-        nome = request.form.get("nome")
-        db.insert({"nome": nome})
-    posts = db.all()
-    return render_template("sobre.html", nome=nome, posts=posts)
+        position_x = request.form.get("position_x")
+        position_y = request.form.get("position_y")
+        position_z = request.form.get("position_z")
+        position_r = request.form.get("position_r")
+        db.insert({"position_x": position_x, "position_y": position_y, "position_z": position_z, "position_r": position_r})
+        print("Caminho cadastrado com sucesso!")
+    return redirect(url_for('index'))
+
+# rota para mostrar todas as posições cadastradas
+@app.route("/home")
+def home():
+    caminhos = db.all()
+    return render_template("home.html", caminhos=caminhos)
+
+# rota para deletar um grupo de posição
+@app.route("/delete/<int:id>")
+def delete(id):
+    db.remove(doc_ids=[id])
+    return redirect(url_for('home'))
+
+# rota para atualizar um grupo de posição
+@app.route("/update/<int:id>")
+def update(id):
+    caminho = db.get(doc_id=id)
+    return render_template("update.html", caminho=caminho)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
---
+++ readme.md
@@ -0,0 +1,14 @@
+para executar esse projeto vc deve instalar as dependencias dele, para isso execute na raiz do projeto:
+pip install -r requirements.txt
+
+agora rode o arquivo principal:
+python main.py
+
+agora basta clicar na url que é fornecida no terminal e usar a aplicação
+
+sobre os diretorios
+projeto-web contem
+- static com o css
+- templates com o html
+- caminhos.json = db
+- main.py =servidor
